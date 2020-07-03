71dbd74f4f566f7575b7314d717d59626a34dba7
Stefan4012IT <42207092+Stefan4012IT@users.noreply.github.com>
2020-07-03 16:30:28 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,122 @@
+"use strict";
+
+/*
+document.addEventListener("click", myFunction)
+
+
+function myFunction() {
+    alert(name);
+}
+*/
+/*var myData = JSON.parse(mapaData)
+var name = myData[0].cityName
+*/
+/*
+document.getElementById("myDoc").addEventListener("change", function() {
+    var file_to_read = document.getElementById("myDoc").files[0];
+    var fileread = new FileReader();
+    fileread.onload = function(e) {
+        var content = e.target.result;
+        
+        var intern = JSON.parse(content);
+        console.log(intern);
+    };
+    fileread.readAsText(file_to_read);
+})
+*/
+/*
+var myInit = {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'applicatoin/json'
+    },
+    mode: 'cors',
+    cache: 'default'
+};
+
+let myRequest = new Request("./mapaData.json", myInt);
+*/
+
+/*<a href="#blabla" class="city__close">&times;</a>
+                        <h1 class="cityName" id="cityName">${city.cityName}</h1>
+                        <img class="profilPhoto" src="img/tulip.jpg" alt="Amsterdam" id="profilPhoto">
+                        <p class="population"><span>Population:</span>${city.population}</p>
+                        <p class="about">${city.about}</p>
+                        <p class="links" id="link"><span>Links: </span></p>*/
+
+function sendID(id) {
+        fetch("./mapaData.json")
+        .then((resp) => resp.json())
+        .then((data) => {
+            data.forEach(function(city){
+                if(id === city.cityName){
+                    document.getElementById('cities__info').innerHTML = 
+                        `<a href="#blabla" class="city__close">&times;</a>
+                        <h1 class="cityName" id="cityName">${city.cityName}</h1>
+                        <div class="photo__galery" id="photo__galery">
+                        </div>
+                        <p class="population"><span>Population:</span>${city.population}</p>
+                        <p class="about">${city.about}</p>
+                        <p class="links" id="link"><span>Links: </span></p>`;
+                    
+                    for (var i = 0; i < city.links.length; i++){
+                        var obj = city.links[i];
+                            for (var key in obj){
+                                var attrName = key;
+                                var attrValue = obj[key];
+                                document.getElementById("link").insertAdjacentHTML("beforeend", `<a href="${attrValue}" class="webLinks" id="webLinks">${attrName} ||| </a>`)
+                            }
+                        }
+                    for (var z = 0; z < city.photos.length; z++){
+                        var href = city.photos[z];
+                                document.getElementById("photo__galery").insertAdjacentHTML("beforeend", `
+                                <a href="#openPicture" id="picture" onclick="sendHref('${href}')">
+                                    <img class="profilPhoto" src="./${href}" alt="${city.cityName}_${z} id="profilPhoto">
+                                </a>
+                                
+                                `);
+                                
+                            
+                        }
+                    
+                    
+                    }
+                    
+                
+                    console.log();
+                    console.log(city.population);
+                })
+            })
+        
+    
+        console.log(id);
+}
+
+function sendHref(img){
+    var pic = img;
+    document.getElementById('openPicture').innerHTML = 
+                        `<a href="#cities__info" class="city__close--white" onclick="sendID('Amsterdam')">&times;</a>
+                        <div class="img__content">
+                            <img src="./${pic}" alt="img main" class="img__full">
+                        </div>
+                        `;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
